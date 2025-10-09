0ed5d0954a4f9c6a62f2142e3556e50e79c3b43b
Roberto Almeida <jesusroberto870@gmail.com>
2025-10-09 14:43:59 -0300

    Correção exercícios

--- Carro.py
+++ Carro.py
@@ -11,35 +11,27 @@ class Carro:
         return self.__marca
     def set_marca(self, nova_marca):
         self.__marca = nova_marca
-    
     # Getter e Setter - modelo
     def get_modelo(self):
         return self.__modelo
-
     def set_modelo(self, novo_modelo):
         self.__modelo = novo_modelo
-
     # Getter e Setter - ano
     def get_ano(self):
         return self.__ano
-
     def set_ano(self, novo_ano):
         if novo_ano > 1885:  # primeiro carro inventado em 1886
             self.__ano = novo_ano
         else:
             print("Ano inválido para um carro!")
-
     # Getter e Setter - cor
     def get_cor(self):
         return self.__cor
-
     def set_cor(self, nova_cor):
         self.__cor = nova_cor
-
     # Getter e Setter - preço
     def get_preco(self):
         return self.__preco
-
     def set_preco(self, novo_preco):
         if novo_preco >= 0:
             self.__preco = novo_preco
@@ -72,6 +64,6 @@ print("Preço:", carro1.get_preco())
 
 # Testando validações
 print("\nTestando valores inválidos:")
-carro1.set_ano(2000)   # inválido
-carro1.set_preco(1000) # inválido
+#carro1.set_ano(2000)   # inválido
+#carro1.set_preco(1000) # inválido
 
